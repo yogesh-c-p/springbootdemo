b59bc9eb92ff6df500dd0a33934cd97b39c1412c
yogesh <yogeshachangolliy@gmail.com>
2023-06-13 13:03:51 +0530

    springboot app

---
+++ src/main/java/com/staragileexample/springboot/demo/Application.java
@@ -0,0 +1,13 @@
+package com.staragileexample.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
